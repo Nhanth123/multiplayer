bf09d3777c8d95d88232d3edc4016ac4b434b530
nhan.trinh <hongnhan50@gmail.com>
2024-09-19 12:44:40 +0700

    Feature: create a host for game-play

---
+++ scripts/menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_host_btn_pressed() -> void:
+	Lobby.create_game()
+
+
+func _on_join_btn_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_start_btn_pressed() -> void:
+	pass # Replace with function body.
