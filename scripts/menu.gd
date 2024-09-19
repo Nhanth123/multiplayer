668300094e127174421f742aeaeb00cb63b8078c
nhan.trinh <hongnhan50@gmail.com>
2024-09-19 17:00:00 +0700

    Feature: joining a host

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,7 +1,11 @@
 extends Control
 
+@export var ip_line_edit: LineEdit
+@export var status_label: Label
+
+
 func _ready() -> void:
-	pass
+	multiplayer.connection_failed.connect(_on_connection_failed)
 
 func _process(delta: float) -> void:
 	pass
@@ -11,8 +15,11 @@ func _on_host_btn_pressed() -> void:
 
 
 func _on_join_btn_pressed() -> void:
-	pass # Replace with function body.
-
+	Lobby.join_game(ip_line_edit.text)
+	status_label.text = "Connecting..."
 
 func _on_start_btn_pressed() -> void:
 	pass # Replace with function body.
+
+func _on_connection_failed():
+	status_label.text = "Failed to connect"
